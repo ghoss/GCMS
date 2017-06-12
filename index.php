752c240f9615c377b369f901b05975eac8251726
Guido <ghoss@users.noreply.github.com>
2017-06-13 00:32:43 +0200

    Don't redirect to login if only browsing

--- index.php
+++ index.php
@@ -7,23 +7,7 @@
 //
 // Created: 15.08.2016 23:43:40 GMT+2
 //=============================================================================================
-// Copyright (C) 2016 by Guido Hoss
-//
-// GCMS is free software: you can redistribute it and/or 
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation, either version 3
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public
-// License along with this program.  If not, see
-// <http://www.gnu.org/licenses/>.
-//
-// Git repository home: <https://github.com/ghoss/GCMS>
+// Copyright: (c) 2016 by Guido Hoss. All Rights Reserved.
 //=============================================================================================
 
 require_once('config/gcms.cfg');
@@ -48,7 +32,7 @@ if ($action == '')
 if (isset($_COOKIE[SESSION_COOKIE]))
 {
 	User::validate($_COOKIE[SESSION_COOKIE], $timeout);
-	if ($timeout && ($action != 'login'))
+	if ($timeout && ($action != 'login') && ($action != 'show'))
 	{
 		// The session was valid, but it timed out. Save session state and redirect to login
 		session_start();
