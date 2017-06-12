323b1ea8c607dce8eb1a5f1f05b3e76f6125f644
Guido <ghoss@users.noreply.github.com>
2017-06-13 00:38:00 +0200

    Don't redirect to login if only browsing
    Don't redirect expired sessions to login page if last action was browsing.

--- index.php
+++ index.php
@@ -7,7 +7,23 @@
 //
 // Created: 15.08.2016 23:43:40 GMT+2
 //=============================================================================================
-// Copyright: (c) 2016 by Guido Hoss. All Rights Reserved.
+// Copyright (C) 2016 by Guido Hoss
+//
+// GCMS is free software: you can redistribute it and/or 
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
+//
+// Git repository home: <https://github.com/ghoss/GCMS>
 //=============================================================================================
 
 require_once('config/gcms.cfg');
